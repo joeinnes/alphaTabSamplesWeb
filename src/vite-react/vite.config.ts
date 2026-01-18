01a053f88bcda1c2a18b9debac517a6a4fb177d0
Joe Innes <joe@joeinn.es>
2026-01-18 14:23:02 +0100

    Update alphaTab import path in vite.config.ts

--- src/vite-react/vite.config.ts
+++ src/vite-react/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { alphaTab } from "@coderline/alphatab/vite";
+import { alphaTab } from "@coderline/alphatab-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
